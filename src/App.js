add3358f30708b6ddc2f83b5c1a7c2a8087147b8
mbrennerr <marciobrennerbusiness@gmail.com>
2021-08-29 19:20:31 -0300

    Requisito 1 vai na fe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import {
+  MovieList,
+  NewMovie,
+  MovieDetails,
+  EditMovie,
+  NotFound,
+} from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="*" component={ NotFound } />
+      </Switch>
+    </Router>
   );
 }
 
